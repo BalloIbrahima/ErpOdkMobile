e94ba2039743bc968b28f4268f952b655e75b7f9
RootAba <abassemaiga403@gmail.com>
2022-09-24 15:55:29 +0000

    page ajout entite

---
+++ PlatformErpOdc/src/app/ajout-entite/ajout-entite.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajout-entite',
+  templateUrl: './ajout-entite.page.html',
+  styleUrls: ['./ajout-entite.page.scss'],
+})
+export class AjoutEntitePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- PlatformErpOdc/src/app/app-routing.module.ts
+++ PlatformErpOdc/src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'ajout-entite',
+    loadChildren: () => import('./ajout-entite/ajout-entite.module').then( m => m.AjoutEntitePageModule)
   },
+
 ];
 
 @NgModule({

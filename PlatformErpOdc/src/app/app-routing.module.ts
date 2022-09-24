fee761365ed6b5cef0233d34d1aae2207307124b
Abdoulaye Sy <doulsy2021@gmail.com>
2022-09-24 20:18:17 +0000

    Creation d'activité

--- PlatformErpOdc/src/app/app-routing.module.ts
+++ PlatformErpOdc/src/app/app-routing.module.ts
@@ -15,6 +15,11 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'creer-activite',
+    loadChildren: () => import('./creer-activite/creer-activite.module').then( m => m.CreerActivitePageModule)
+  },
+
 ];
 
 @NgModule({

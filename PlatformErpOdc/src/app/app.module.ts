d065b062902f787d526f32a568a1526b1f5fb9b8
kdiawara96 <karimdiawara96@gmail.com>
2022-09-24 18:22:35 +0000

    version betta2 accueil

--- PlatformErpOdc/src/app/app.module.ts
+++ PlatformErpOdc/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AccueilPipe } from './accueil.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AccueilPipe],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],

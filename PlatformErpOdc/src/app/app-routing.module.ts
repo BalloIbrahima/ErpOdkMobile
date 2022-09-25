57bfe98f3af41c465ba238914f99fb9753b1964e
Dmchez33 <iddembele206@gmail.com>
2022-09-25 18:26:05 +0000

    ajout du chemin creer activiter dans app-routing.module.ts

--- PlatformErpOdc/src/app/app-routing.module.ts
+++ PlatformErpOdc/src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'activite',
+    loadChildren: () => import('./creer-activite/creer-activite.module').then(m => m.CreerActivitePageModule)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'

f7d47525c7daebc8b55d011cb3efb9b0155319eb
jssagara <jeansagara272@gmail.com>
2022-09-26 15:27:06 +0000

    page

--- PlatformErpOdc/src/app/postulants-tire/postulants-tire.module.ts
+++ PlatformErpOdc/src/app/postulants-tire/postulants-tire.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { PostulantsTirePageRoutingModule } from './postulants-tire-routing.module';
 
@@ -13,7 +14,8 @@ import { PostulantsTirePage } from './postulants-tire.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    PostulantsTirePageRoutingModule
+    PostulantsTirePageRoutingModule,
+    NgxPaginationModule
   ],
   declarations: [PostulantsTirePage]
 })

06cc754beefa69b3361ad65774da8513f8865da5
jssagara <jeansagara272@gmail.com>
2022-09-26 19:39:39 +0000

    papa

--- PlatformErpOdc/src/app/postulants-tire/postulants-tire.page.ts
+++ PlatformErpOdc/src/app/postulants-tire/postulants-tire.page.ts
@@ -6,19 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./postulants-tire.page.scss'],
 })
 export class PostulantsTirePage implements OnInit {
+  // La pagination
   accueil = [
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
-    {nom: "dembele", prenom:"idrissa"},
+    {nom: "Mr Dembele", email:"idrissa@gmail.com"},
+    {nom: "Mr SAGARA", email:"jean@gmail.com"},
+    {nom: "Mr DIALLO", email:"abdoulaye@gmail.com"},
+    {nom: "Mr DJENIKA", email:"abocar@gmail.com"},
+    {nom: "Mr DJIGUIBA", email:"barema@gmail.com"},
+    {nom: "Mr BORE", email:"souleymane@gmail.com"},
+    {nom: "Mr DIAWARA", email:"karim@gmail.com"},
+    {nom: "Mr BALLO", email:"ibrahim@gmail.com"},
+    {nom: "Mr MAIGA", email:"abasse@gmail.com"},
+    {nom: "Mr SENOU", email:"kalifa@gmail.com"},
+    {nom: "Mr MALLE", email:"allassane@gmail.com"},
+    {nom: "Mr TRAORE", email:"mary@gmail.com"},
+   
   ]
  
     

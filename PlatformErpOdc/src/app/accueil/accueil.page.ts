f4ff4a977367d5f6f3aeb22a8a85322220475a97
kdiawara96 <karimdiawara96@gmail.com>
2022-09-26 17:35:20 +0000

    nouvelle carrousel

--- PlatformErpOdc/src/app/accueil/accueil.page.ts
+++ PlatformErpOdc/src/app/accueil/accueil.page.ts
@@ -12,4 +12,38 @@ export class AccueilPage implements OnInit {
   ngOnInit() {
   }
 
+
+  change():any{
+    
+  var
+  carousel = document.querySelector('.carousel'),
+  figure = carousel.querySelector('figure'),
+  nav = carousel.querySelector('nav'),
+  numImages = figure.childElementCount,
+  theta =  2 * Math.PI / numImages,
+  currImage = 0;
+  
+  nav.addEventListener('click', onClick, true);
+  
+  function onClick(e) {
+  e.stopPropagation();
+  
+  var t = e.target;
+  
+  
+  if (t.tagName.toUpperCase() != 'BUTTON')
+  return;
+  
+  if (t.classList.contains('suiv')) {
+  currImage++;
+  }
+  else if(t.classList.contains('pre')) {
+  currImage--;
+  }
+  
+  
+  figure.style.transform = `rotateY(${currImage * -theta}rad)`;
+  }
+  }
+
 }

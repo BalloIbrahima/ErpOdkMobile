5fad31578d51fd74fb943332196c289b47b9f241
amalle <alassanemalle733@gmail.com>
2022-09-30 21:42:59 +0000

    pull karim

--- PlatformErpOdc/src/app/app-routing.module.ts
+++ PlatformErpOdc/src/app/app-routing.module.ts
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  // {
-  //   path: '',
-  //   redirectTo: 'detailslistes',
-  //   pathMatch: 'full'
-  // },
-  {
-    path: '',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-
-  {
-
-    path: 'profil',
-    loadChildren: () => import('./profil/profil.module').then( m => m.ProfilPageModule)
-
-    path: 'personnels',
-    loadChildren: () => import('./personnel/personnel.module').then( m => m.PersonnelPageModule)
-  },
-
-  {
-    path: 'nouveau-personnel',
-    loadChildren: () => import('./nouveaupersonnel/nouveaupersonnel.module').then( m => m.NouveaupersonnelPageModule)
-  },
-  {
-    path: 'detailpersonnel',
-    loadChildren: () => import('./detailpostulant/detailpostulant.module').then( m => m.DetailpostulantPageModule)
-
-  },
-  // {
-  //   path: 'profile',
-  //   loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
-  // },
-
-
-
-
-
-
-
-
-
-];
-=======
-
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -61,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'profil',
     pathMatch: 'full'
   },
   {
@@ -84,7 +31,10 @@ const routes: Routes = [
     path: 'accueil',
     loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
   },
->>>>>>> karimSave
+  {
+    path: 'profil',
+    loadChildren: () => import('./profil/profil.module').then( m => m.ProfilPageModule)
+  },
 
 
 ];

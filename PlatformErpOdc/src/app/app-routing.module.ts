4801def4808d44b3eb3bd5f9a49588f26244a1ab
kdiawara96 <karimdiawara96@gmail.com>
2022-09-30 21:11:11 +0000

    merge 1 karim

--- PlatformErpOdc/src/app/app-routing.module.ts
+++ PlatformErpOdc/src/app/app-routing.module.ts
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-
-  {
-    path: 'personnels',
-    loadChildren: () => import('./personnel/personnel.module').then( m => m.PersonnelPageModule)
-  },
-
-  {
-    path: 'nouveau-personnel',
-    loadChildren: () => import('./nouveaupersonnel/nouveaupersonnel.module').then( m => m.NouveaupersonnelPageModule)
-  },
-  {
-    path: 'detailpersonnel',
-    loadChildren: () => import('./detailpostulant/detailpostulant.module').then( m => m.DetailpostulantPageModule)
-  },
-  // {
-  //   path: 'profile',
-  //   loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
-  // },
-
-
-
-];
-=======
 
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
@@ -50,7 +9,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'profil',
     pathMatch: 'full'
   },
   {
@@ -68,11 +27,15 @@ const routes: Routes = [
   {
     path: 'detailpersonnel',
     loadChildren: () => import('./detailpostulant/detailpostulant.module').then( m => m.DetailpostulantPageModule)
-  },  {
+  },
+  {
     path: 'accueil',
     loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
   },
->>>>>>> karimSave
+  // {
+  //   path: 'profil',
+  //   loadChildren: () => import('./profil/profil.module').then( m => m.ProfilPageModule)
+  // },
 
 
 ];

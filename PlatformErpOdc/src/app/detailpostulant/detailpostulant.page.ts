1e60e60b52092142ef79d46984629917df7b1bb4
mttraore <marytra22@gmail.com>
2022-10-03 18:55:13 +0000

    les modifs final

--- PlatformErpOdc/src/app/detailpostulant/detailpostulant.page.ts
+++ PlatformErpOdc/src/app/detailpostulant/detailpostulant.page.ts
@@ -137,4 +137,28 @@ export class DetailpostulantPage implements OnInit {
     // }
   }
 
+
+
+
+
+  // @ViewChild(IonModal) modal: IonModal;
+
+  // message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  // name: string;
+
+  // cancel() {
+  //   this.modal.dismiss(null, 'cancel');
+  // }
+
+  // confirm() {
+  //   this.modal.dismiss(this.name, 'confirm');
+  // }
+
+  // onWillDismiss(event: Event) {
+  //   const ev = event as CustomEvent<OverlayEventDetail<string>>;
+  //   if (ev.detail.role === 'confirm') {
+  //     this.message = `Hello, ${ev.detail.data}!`;
+  //   }
+  // }
+
 }

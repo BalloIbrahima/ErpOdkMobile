2e6096553f0bda943150bd78616abf03de928590
Idrissa Dembélé <iddembele206@gmail.com>
2022-10-05 09:59:20 +0000

    another commit

--- PlatformErpOdc/src/app/role/role.page.ts
+++ PlatformErpOdc/src/app/role/role.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RoleService } from '../services/role/role.service';
 
 @Component({
   selector: 'app-role',
@@ -6,8 +7,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./role.page.scss'],
 })
 export class RolePage implements OnInit {
+  Utilisateur: any;
+  TousLesRole: any;
 
-  constructor() { }
+  // createEntite: any;
+  // updateEntite: any;
+  // readEntite: any;
+  // deleteEntite: any;
+
+  // createActivite: any;
+  // updateActivite: any;
+  // readActivite: any;
+  // deleteActivite: any;
+
+  // createSalle: any;
+  // updateSalle: any;
+  // readSalle: any;
+  // deleteSalle: any;
+
+  // createAouP: any;
+  // updateAouP: any;
+  // readAouP: any;
+  // deleteAouP: any;
+
+  // createDesignation: any;
+  // updateDesignation: any;
+  // readDesignation: any;
+  // deleteDesignation: any;
+
+  // createEtat: any;
+  // updateEtat: any;
+  // readEtat: any;
+  // deleteEtat: any;
+
+  // createFormatEmail: any;
+  // updateFormatEmail: any;
+  // readFormatEmail: any;
+  // deleteFormatEmail: any;
+
+  // createIntervenant: any;
+  // updateIntervenant: any;
+  // readIntervenant: any;
+  // deleteIntervenant: any;
+
+  // createListPostulant: any;
+  // updateListPostulant: any;
+  // readListPostulant: any;
+  // deleteListPostulant: any;
+
+  // createPostulant: any;
+  // updatePostulant: any;
+  // readPostulant: any;
+  // deletePostulant: any;
+
+  // createPresence: any;
+  // updatePresence: any;
+  // readPresence: any;
+  // deletePresence: any;
+
+  // createRole: any;
+  // updateRole: any;
+  // readRole: any;
+  // deleteRole: any;
+
+  // createTache: any;
+  // updateTache: any;
+  // readTache: any;
+  // deleteTache: any;
+
+  // createTirage: any;
+  // updateTirage: any;
+  // readTirage: any;
+  // deleteTirage: any;
+
+  // createUtilisateur: any;
+  // updateUtilisateur: any;
+  // readUtilisateur: any;
+  // deleteUtilisateur: any;
+
+  // createTypeActivite: any;
+  // updateTypeActivite: any;
+  // readTypeActivite: any;
+  // deleteTypeActivite: any;
+
+  constructor(private seriveRole: RoleService) { }
   // eslint-disable-next-line @typescript-eslint/member-ordering
   info = [
     {
@@ -22,9 +105,50 @@ export class RolePage implements OnInit {
       nom: 'Hamadoun Kaou DIALLO',
       status: 'Responsable',
     },
+    {
+      entite: 'Orange Digital Kalnso',
+      descrription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam eligendi harum tempora expedita sed officia molestiasquas eius culpa. Reprehenderit nam exercitationem error quidem hic molestiae, id ut illo consequuntur.',
+      nom: 'Hamadoun Kaou DIALLO',
+      status: 'Responsable',
+    },
+    {
+      entite: 'Orange Digital Kalnso',
+      descrription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam eligendi harum tempora expedita sed officia molestiasquas eius culpa. Reprehenderit nam exercitationem error quidem hic molestiae, id ut illo consequuntur.',
+      nom: 'Hamadoun Kaou DIALLO',
+      status: 'Responsable',
+    },
+    {
+      entite: 'Orange Digital Kalnso',
+      descrription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam eligendi harum tempora expedita sed officia molestiasquas eius culpa. Reprehenderit nam exercitationem error quidem hic molestiae, id ut illo consequuntur.',
+      nom: 'Hamadoun Kaou DIALLO',
+      status: 'Responsable',
+    },
+    {
+      entite: 'Orange Digital Kalnso',
+      descrription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam eligendi harum tempora expedita sed officia molestiasquas eius culpa. Reprehenderit nam exercitationem error quidem hic molestiae, id ut illo consequuntur.',
+      nom: 'Hamadoun Kaou DIALLO',
+      status: 'Responsable',
+    },
+    {
+      entite: 'Orange Digital Kalnso',
+      descrription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam eligendi harum tempora expedita sed officia molestiasquas eius culpa. Reprehenderit nam exercitationem error quidem hic molestiae, id ut illo consequuntur.',
+      nom: 'Hamadoun Kaou DIALLO',
+      status: 'Responsable',
+    },
+    {
+      entite: 'Orange Digital Kalnso',
+      descrription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam eligendi harum tempora expedita sed officia molestiasquas eius culpa. Reprehenderit nam exercitationem error quidem hic molestiae, id ut illo consequuntur.',
+      nom: 'Hamadoun Kaou DIALLO',
+      status: 'Responsable',
+    },
   ];
 
   ngOnInit() {
+    this.Utilisateur = JSON.parse(localStorage.getItem('utilisateur'));
+    this.seriveRole.getAllRole(this.Utilisateur.login, this.Utilisateur.password).subscribe(data =>{
+      this.TousLesRole = data.data
+      console.log(this.TousLesRole)
+    })
   }
 
 }
---
+++ PlatformErpOdc/src/app/services/role/role.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+  env = environment
+  constructor(private http: HttpClient) { }
+
+  // methode permettant de recuperer tous les droits
+
+  getAllDroit(login: string, password: string): Observable<any>{
+    const data:FormData=new FormData();
+    const user=[
+      {
+        "login":login,
+        "password":password
+      }]
+      
+    data.append('user', JSON.stringify(user).slice(1,JSON.stringify(user).lastIndexOf(']')));
+    return this.http.post(`${this.env.api}/admin/droit/Getall`,data)
+
+  }
+
+  getAllRole(login: string, password: string): Observable<any>{
+    const data:FormData=new FormData();
+    const user=[
+      {
+        "login":login,
+        "password":password
+      }]
+      
+    data.append('user', JSON.stringify(user).slice(1,JSON.stringify(user).lastIndexOf(']')));
+    return this.http.post(`${this.env.api}/admin/role/getAll`,data)
+
+  }
+}

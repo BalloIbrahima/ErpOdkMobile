d84722de722ddce06e4e82fefc603892e4037c1c
attraore <taoua1986@gmail.com>
2022-10-07 14:54:32 +0000

    abass

--- PlatformErpOdc/src/app/personnel/personnel.page.ts
+++ PlatformErpOdc/src/app/personnel/personnel.page.ts
@@ -10,22 +10,9 @@ export class PersonnelPage implements OnInit {
   responsive = true
 
   users :any
-  // [
-  //   {nom: "Mr Dembele", email:"idrissa@gmail.com"},
-  //   {nom: "Mr SAGARA", email:"jean@gmail.com"},
-  //   {nom: "Mr DIALLO", email:"abdoulaye@gmail.com"},
-  //   {nom: "Mr DJENIKA", email:"abocar@gmail.com"},
-  //   {nom: "Mr DJIGUIBA", email:"barema@gmail.com"},
-  //   {nom: "Mr BORE", email:"souleymane@gmail.com"},
-  //   {nom: "Mr DIAWARA", email:"karim@gmail.com"},
-  //   {nom: "Mr BALLO", email:"ibrahim@gmail.com"},
-  //   {nom: "Mr MAIGA", email:"abasse@gmail.com"},
-  //   {nom: "Mr SENOU", email:"kalifa@gmail.com"},
-  //   {nom: "Mr MALLE", email:"allassane@gmail.com"},
-  //   {nom: "Mr TRAORE", email:"mary@gmail.com"},
-  // ]
   a!:any;
   Utilisateur:any;
+  
 
   constructor(private userService:UtilisateurService) { }
 
--- PlatformErpOdc/src/app/profil/profil.page.ts
+++ PlatformErpOdc/src/app/profil/profil.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilPage implements OnInit {
 
-  constructor() { }
+// 
+nom:any;
+prenom:any;
+entité:any;
+persoEnt:any;
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
   }

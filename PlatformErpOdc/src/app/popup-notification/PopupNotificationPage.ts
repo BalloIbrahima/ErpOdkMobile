f3c209fd5ea8e9ab9e13df1aea0a8c6bb6915f25
mttraore <marytra22@gmail.com>
2022-10-07 19:19:35 +0000

    mes modifs

---
+++ PlatformErpOdc/src/app/popup-notification/PopupNotificationPage.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PopoverController } from '@ionic/angular';
+import { NotificationService } from '../services/notification/notification.service';
+
+
+@Component({
+  selector: 'app-popup-notification',
+  templateUrl: './popup-notification.page.html',
+  styleUrls: ['./popup-notification.page.scss'],
+})
+export class PopupNotificationPage implements OnInit {
+  p: number = 1;
+  Utilisateur: any;
+  notif: any;
+  notifTotal: any;
+  id: any;
+  dateNotif: any;
+  descriptionNotif: any;
+  titreNotif: any;
+  NomCreation: any;
+  nomActivite: any;
+  nomCreateurActivite: any;
+  prenomCreateurActivite: any;
+  idNotif: any;
+
+
+  constructor(private popoverCtrl: PopoverController, private notificationService: NotificationService,
+    private router: Router, private route: ActivatedRoute) { }
+  // salleactivites=[
+  //   {
+  //     user:"Fatoumata Kalogo",
+  //     activite:"activité",
+  //   },
+  //   {
+  //     user:"Fatoumata Kalogo",
+  //     activite:"activité",
+  //   },
+  //   {
+  //     user:"Fatoumata Kalogo",
+  //     activite:"activité",
+  //   },
+  //   {
+  //     user:"Fatoumata Kalogo",
+  //     activite:"activité",
+  //   },
+  //   {
+  //     user:"Fatoumata Kalogo",
+  //     activite:"activité",
+  //   },
+  //   {
+  //     user:"Fatoumata Kalogo",
+  //     activite:"activité",
+  //   },
+  // ]
+  close() {
+    this.popoverCtrl.dismiss();
+  }
+
+  ngOnInit() {
+    // this.idNotif = this.route.snapshot.params['id'];
+    this.Utilisateur = JSON.parse(localStorage.getItem('utilisateur'));
+
+    this.notificationService.GetAllNotif(this.Utilisateur.login, this.Utilisateur.password).subscribe(data => {
+      this.notif = data.data;
+      console.log("data notif" + this.notif);
+      this.notifTotal = data.data.length;
+      console.log("data notif" + this.notif);
+    });
+
+
+    this.notificationService.GetNotificationById(this.Utilisateur.login, this.Utilisateur.password, this.id).subscribe(data => {
+      this.notif = data.data;
+      console.log("Les notifications" + this.notif);
+      this.dateNotif = this.notif.datenotif;
+      this.descriptionNotif = this.notif.description;
+      this.titreNotif = this.notif.titre;
+      this.nomActivite = this.notif.idactivite.nom;
+      this.nomCreateurActivite = this.notif.idactivite.createur.nom;
+      this.prenomCreateurActivite = this.notif.idactivite.createur.prenom;
+
+    });
+
+  }
+
+
+
+}
--- PlatformErpOdc/src/app/services/notification/notification.service.ts
+++ PlatformErpOdc/src/app/services/notification/notification.service.ts
@@ -10,6 +10,7 @@ export class NotificationService {
   env=environment;
   constructor(public http:HttpClient) { }
 
+  //La fonction pour recuperer tout les notifications
   GetAllNotif(login:String, password:String):Observable<any>{
     const data:FormData=new FormData();
     const user=[{"login":login,"password":password}]
@@ -17,5 +18,18 @@ export class NotificationService {
   
     return this.http.post(`${this.env.api}/notification/allnotification`, data);
   }
+
+  
+//La fonction pour recuperer une notification
+GetNotificationById(login:String, password:String,id:number):Observable<any>{
+  const data:FormData=new FormData();
+  const notification=[
+    {
+      "login":login,
+      "password":password
+    }];
+    data.append('notification' , JSON.stringify(notification).slice(1,JSON.stringify(notification).lastIndexOf(']')));
+    return this.http.post(`${this.env.api}/notification/get/notification/${id}`, data);
+}
 
 }

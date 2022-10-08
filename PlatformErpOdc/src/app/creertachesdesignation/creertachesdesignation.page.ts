7973943188f294ae4718a7ee2aa0145380a62079
amalle <alassanemalle733@gmail.com>
2022-10-08 14:00:18 +0000

    Avant pull ballo

--- PlatformErpOdc/src/app/creertachesdesignation/creertachesdesignation.page.ts
+++ PlatformErpOdc/src/app/creertachesdesignation/creertachesdesignation.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EntiteService } from '../services/entite/entite.service';
+import { SalleServiceService } from '../services/salles/salle-service.service';
 import { TachedesignationService } from '../services/tahedesignations/tachedesignation.service';
 
 @Component({
@@ -7,20 +9,43 @@ import { TachedesignationService } from '../services/tahedesignations/tachedesig
   styleUrls: ['./creertachesdesignation.page.scss'],
 })
 export class CreertachesdesignationPage implements OnInit {
-
+  Entites:any;
   Utilisateur : any;
-  etatselect : string;
+  sallesDipo:any
+  // etatselect : string;
   nom : string;
   etat: boolean;
-  if (etatselect = "Actif") {
-    this.etat = true
-  } elsif (etatselect = "Inactif") {
-    this.etat = false
-  }
-  constructor( private tachedesign : TachedesignationService) { }
+  // if (etatselect = "Actif") {
+  //   this.etat = true
+  // } elsif (etatselect = "Inactif") {
+  //   this.etat = false
+  // }
+  constructor( private tachedesign : TachedesignationService ,private salleService: SalleServiceService,private entiteService:EntiteService) { }
 
   ngOnInit() {
-    this.Utilisateur = JSON.stringify(localStorage.getItem('utilisateur'));
+    
+    this.Utilisateur=JSON.parse(localStorage.getItem('utilisateur')) ;
+
+    this.salleService.getSalleDisponible(this.Utilisateur.login,this.Utilisateur.password).subscribe(data=>{
+      this.sallesDipo=data.data;
+      // this.sallesDipoLength = data.data.length
+      console.log(data.data)
+    });
+
+   
+    
+    
+
+    this.entiteService.getAllEntites(this.Utilisateur.login, this.Utilisateur.password).subscribe(retour=>{
+      this.Entites=retour.data
+      console.log(this.Entites)
+    })
+
+
+
+
+
+
   }
 
   creerDesignation() {

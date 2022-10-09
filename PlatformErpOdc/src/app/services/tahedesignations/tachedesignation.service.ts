5247eec1df5c53e4d5965ba03bcc9165fcccbc14
kdiawara96 <karimdiawara96@gmail.com>
2022-10-09 16:42:52 +0000

    fait du binding créer tâche et désignation

--- PlatformErpOdc/src/app/services/tahedesignations/tachedesignation.service.ts
+++ PlatformErpOdc/src/app/services/tahedesignations/tachedesignation.service.ts
@@ -51,7 +51,7 @@ getEtat(){
       
       data.append('user', JSON.stringify(user).slice(1, JSON.stringify(user).lastIndexOf(']')));
   
-      return this.http.post(`${this.env.api}/..............`, data);
+      return this.http.post(`${this.env.api}​/designation​/Designation​/GetAll`, data);
   
       }
 

7734bf1f6675a6365ccb1a0dfff0e4c28b5556de
kdiawara96 <karimdiawara96@gmail.com>
2022-10-10 08:26:43 +0000

    success 10/10/2022

--- PlatformErpOdc/src/app/app-routing.module.ts
+++ PlatformErpOdc/src/app/app-routing.module.ts
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'accueil',
-=======
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AcceuilGuard } from './guard/acceuil/acceuil.guard';
-
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'dashboard',
->>>>>>> karimSave
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-<<<<<<< HEAD
-    path: 'personnels',
-    loadChildren: () => import('./personnel/personnel.module').then( m => m.PersonnelPageModule)
-  },
-  {
-    path: 'nouveau-personnel',
-    loadChildren: () => import('./nouveaupersonnel/nouveaupersonnel.module').then( m => m.NouveaupersonnelPageModule)
-  },
-  {
-    path: 'detailpersonnel',
-    loadChildren: () => import('./detailpostulant/detailpostulant.module').then( m => m.DetailpostulantPageModule)
-  },
-=======
-=======
-
->>>>>>> karimSave
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AcceuilGuard } from './guard/acceuil/acceuil.guard';
@@ -56,52 +8,11 @@ const routes: Routes = [
     redirectTo: 'dashboard',
     pathMatch: 'full'
   },
-<<<<<<< HEAD
-
-  {
-    path: 'profil',
-    loadChildren: () => import('./profil/profil.module').then( m => m.ProfilPageModule),
-    // canActivate: [AcceuilGuard] 
-  },
-  {
-    path: 'personnel',
-    loadChildren: () => import('./personnel/personnel.module').then( m => m.PersonnelPageModule),
-    // canActivate: [AcceuilGuard] 
-  },
-
->>>>>>> karimSave
-  {
-    path: 'accueil',
-    loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule),
-    // canActivate: [AcceuilGuard] 
-  },
-<<<<<<< HEAD
-  // {
-  //   path: 'profil'
-  //   loadChildren: () => import('./profil/profil.module').then( m => m.ProfilPageModule)
-  //  },
-
-
-];
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
-=======
-=======
->>>>>>> karimSave
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
-
   {
-=======
->>>>>>> karimSave
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule),
     canActivate: [AcceuilGuard]
@@ -114,13 +25,7 @@ export class AppRoutingModule { }
   {
     path: 'forgotpassword1',
     loadChildren: () => import('./forgetpassword1/forgetpassword1.module').then( m => m.Forgetpassword1PageModule)
-<<<<<<< HEAD
-  },
-
-
-=======
   }
->>>>>>> karimSave
 ];
 @NgModule({
   imports: [
@@ -163,9 +68,6 @@ export class AppRoutingModule { }
 //   exports: [RouterModule]
 // })
 
-<<<<<<< HEAD
->>>>>>> karimSave
-=======
 
 
 
@@ -206,4 +108,3 @@ export class AppRoutingModule { }
 
 
 
->>>>>>> karimSave

89a5dbe7367c546dc4bc2d1808742e63ac36dd43
mttraore <marytra22@gmail.com>
2022-10-14 10:27:35 +0000

    update

--- PlatformErpOdc/src/app/detail-tirage-dune-liste/detail-tirage-dune-liste.module.ts
+++ PlatformErpOdc/src/app/detail-tirage-dune-liste/detail-tirage-dune-liste.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+  
 import { IonicModule } from '@ionic/angular';
 
 import { DetailTirageDuneListePageRoutingModule } from './detail-tirage-dune-liste-routing.module';
